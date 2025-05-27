4936653c71778dc2cfe390212b80c0223fddbf45
nandita2291 <sj.nandita@gmail.com>
2025-05-27 18:38:05 +0530

    Fix Tailwind CSS for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true, // Required for static exports to work properly on GitHub Pages
-
-  basePath: isProd ? '/my-portfolio' : '',
-  assetPrefix: isProd ? '/my-portfolio/' : '',
-
   images: {
     unoptimized: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
