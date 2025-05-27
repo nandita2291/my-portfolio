51cdd1a5be114b155665cfde3dea7219c8627b3f
nandita2291 <sj.nandita@gmail.com>
2025-05-27 16:55:28 +0530

    adding files

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true, // Required for static exports to work properly on GitHub Pages
+
+  basePath: isProd ? '/my-portfolio' : '',
+  assetPrefix: isProd ? '/my-portfolio/' : '',
+
+  images: {
+    unoptimized: true,
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
